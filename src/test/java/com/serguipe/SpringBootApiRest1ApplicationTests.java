80a25ce327bfb0d470bb92af8b86849eb51a6591
Sergio Guillen <serguipe28@gmail.com>
2022-02-25 09:26:55 +0100

    Create Project

---
+++ src/test/java/com/serguipe/SpringBootApiRest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serguipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApiRest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
